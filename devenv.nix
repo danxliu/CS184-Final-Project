10663356b381867a60804092e96d6581c911bab4
Daniel Liu <danxliu@protonmail.com>
2026-04-14 16:59:16 -0700

    feat: initial commit, setup cmake and stuff

---
+++ devenv.nix
@@ -0,0 +1,50 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  env.GREET = "devenv";
+  packages = with pkgs; [ 
+    git
+    cmake
+    pkg-config
+    ninja
+    gcc
+    eigen
+    openmesh
+    dbus
+    wayland-scanner
+    wayland
+    wayland-protocols
+    libxkbcommon
+
+    (suitesparse.override {
+      enableCuda = false;
+    })
+    
+    # OpenGL & X11
+    libGL
+    libGLU
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXxf86vm
+  ];
+
+  languages.cplusplus.enable = true;
+
+  env.LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath (with pkgs; [
+    libGL
+    xorg.libX11
+    xorg.libXext
+    xorg.libXinerama
+    xorg.libXcursor
+    xorg.libXrandr
+    xorg.libXi
+  ]);
+
+  enterShell = ''
+    cmake --version
+    git --version
+  '';
+}
